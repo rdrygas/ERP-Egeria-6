0f4c313b2d9bec5b9c1becc531924170fef868e7
Robert Drygas <robert.drygas@gmail.com>
2026-02-20 08:22:45 +0100

    Update ST_zalaczniki_blob_cx_oracle.py
    Added comments for script purpose and Oracle client info.

--- ST/ST_zalaczniki_blob_cx_oracle.py
+++ ST/ST_zalaczniki_blob_cx_oracle.py
@@ -1,3 +1,6 @@
+# Skrypt do pobierania załączników z bazy danych Oracle i zapisywania ich na dysku
+# Oracle Instant Client Downloads https://www.oracle.com/database/technologies/instant-client/downloads.html
+
 import cx_Oracle
 import getpass
 import os
